61f96ef241203396f2c2bdfe7f9557f2848db4ab
ma300000@gmail.com <ma300000@gmail.com>
2019-12-29 22:53:42 +0000

    Name file after the root component defined by the file

--- appointments/test/AppointmentDayView.test.js
+++ appointments/test/AppointmentDayView.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactTestUtils from 'react-dom/test-utils';
-import {Appointment, AppointmentsDayView} from '../src/Appointment.js';
+import {Appointment, AppointmentsDayView} from '../src/AppointmentDayView.js';
 
 describe('Appointment', () => {
   let customer;
